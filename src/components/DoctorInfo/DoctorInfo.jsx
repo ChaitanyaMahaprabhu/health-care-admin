37af3a600f3f8b3e5a55a32271a0f3429b171f73
chaitanya-mahaprabhu-kanini <chaitanya.kanini@gmail.com>
2023-07-01 12:45:09 +0530

    "DoctorInfo" visual layout created with static data.

--- src/components/DoctorInfo/DoctorInfo.jsx
+++ src/components/DoctorInfo/DoctorInfo.jsx
@@ -1,13 +1,23 @@
-import './DoctorInfo.css';
-import { DoctorCardAvailable, DoctorCardUnavailable } from '../DoctorCard/DoctorCard';
+import "./DoctorInfo.css";
+import {
+  DoctorCardAvailable,
+  DoctorCardUnavailable,
+} from "../DoctorCard/DoctorCard";
 const DoctorInfo = () => {
-    return(
-        <>
-            <div id = "wallpaper">
-                
-            </div>
-        </>
-    );
-}
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  return (
+    <>
+      <div id="wall" style={{ overflowY: "scroll" }}>
+        <div id = "infoDesc" className = "frosted">
+            <h1 style = {{fontSize: '3rem'}}>The best in the world, at your service.</h1>
+            <h4>Choose the doctors you want to consult</h4>
+        </div>
+        <div id = "docInfo">
+            {arr.map(value => <DoctorCardAvailable/>)}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export {DoctorInfo};
+export { DoctorInfo };
