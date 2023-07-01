bb81543f6e812a95dc1a9a4f8fdc0fe35ce210d2
chaitanya-mahaprabhu-kanini <chaitanya.kanini@gmail.com>
2023-07-02 04:39:29 +0530

    Tables for doctor information, with provisions for deleting and toggling data created.

--- src/components/AdminPage/AdminPage.jsx
+++ src/components/AdminPage/AdminPage.jsx
@@ -16,18 +16,88 @@ const AdminPage = (props) => {
             </h4>
             <DoubleStats />
 
-            <h4 id="statsText" className="frosted" style = {{marginTop: '1rem'}}>
+            <h4
+              id="statsText"
+              className="frosted"
+              style={{ marginTop: "1rem" }}
+            >
               Patients
             </h4>
             <DoubleStats />
           </div>
 
-          <div style = {{marginTop: '2rem'}}>
+          <div style={{ marginTop: "2rem" }}>
             <h4 id="statsText" className="frosted">
               Average Experience
             </h4>
             <SingleStats />
           </div>
+        </div>
+
+        <div className="container-fluid bg-light" id="getDoctors">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Gender</th>
+                <th>Specialization</th>
+                <th>Years Of Experience</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>
+                  <button className="btn btn-danger">Delete</button>
+                </td>
+                <td>
+                  <button className="btn btn-warning">Toggle Status</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div className="container-fluid frosted" id="getDoctorById">
+          <form>
+            <div className="mb-3">
+              <label htmlFor="doctorID" className="form-label">
+                Doctor ID
+              </label>
+              <input type="text" className="form-control" id="doctorID" />
+            </div>
+          </form>
+          <table className="table mt-5">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Gender</th>
+                <th>Specialization</th>
+                <th>Years Of Experience</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tbody>
+          </table>
         </div>
       </div>
     </>
