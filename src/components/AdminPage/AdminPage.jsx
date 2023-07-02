217241da9eac72b8f4f940852b4be898d1043b03
chaitanya-mahaprabhu-kanini <chaitanya.kanini@gmail.com>
2023-07-02 21:18:39 +0530

    AdminPage - The data now gets displayed in the tables. DELETE and UPDATE functionalities work too.

--- src/components/AdminPage/AdminPage.jsx
+++ src/components/AdminPage/AdminPage.jsx
@@ -8,13 +8,17 @@ import { useContext } from "react";
 import { useState, useEffect } from "react";
 
 const AdminPage = (props) => {
-  const con = useContext(context);
+  const sharedData = useContext(context);
 
   const [doctorId, setDoctorId] = useState("");
   const [patientId, setPatientId] = useState("");
   const [patientIdData, setPatientIdData] = useState([{}]);
   const [doctorIdData, setDoctorIdData] = useState([{}]);
-  const [toggle, setToggle] = useState({});
+  const [doctorToggle, setDoctorToggle] = useState({});
+
+  useEffect(() => {
+
+  }, [sharedData.doctors, sharedData.patients]);
 
   useEffect(() => {
     fetchDoctorById();
@@ -61,6 +65,95 @@ const AdminPage = (props) => {
     else setPatientId(event.target.value);
   };
 
+  const deletePatient = async (id) => {
+    try {
+      const response = await fetch(
+        `https://localhost:7210/api/Patients/${id}`,
+        {
+          method: "DELETE",
+        }
+      );
+      if (response.ok) {
+        console.log("Patient deleted successfully");
+        sharedData.patients.filter((patient) => patient.id !== id);
+      } else {
+        console.error("Error deleting patient:", response.statusText);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const deleteDoctor = async (id) => {
+    try {
+      const response = await fetch(`https://localhost:7210/api/Doctors/${id}`, {
+        method: "DELETE",
+      });
+      if (response.ok) {
+        console.log("Doctor data deleted successfully");
+        sharedData.doctors.filter((doctor) => doctor.id !== id);
+      } else {
+        console.error("Error deleting doctor:", response.statusText);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const updateDoctorToggle = async (id) => {
+    try {
+      const response = await fetch(`https://localhost:7210/api/Doctors/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(doctorToggle),
+      });
+      if (response.ok) {
+        console.log("Doctor status updated successfully");
+        console.log(doctorToggle);
+        sharedData.doctors.map((doctor) =>
+          doctor.id === id ? doctorToggle : doctor
+        );
+      } else {
+        console.error("Error updating doctor status:", response.statusText);
+        console.log(doctorToggle);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    updateDoctorToggle(doctorToggle.id);
+  }, [doctorToggle]);
+
+  const toggleDoctor = (data) => {
+    if (data.status === "active") {
+      setDoctorToggle({
+        id: data.id,
+        name: `${data.name}`,
+        age: data.age,
+        gender: `${data.gender}`,
+        specialization: `${data.specialization}`,
+        experience: data.experience,
+        request: "no",
+        status: "inactive",
+      });
+    } else {
+      setDoctorToggle({
+        id: data.id,
+        name: `${data.name}`,
+        age: data.age,
+        gender: `${data.gender}`,
+        specialization: `${data.specialization}`,
+        experience: data.experience,
+        request: "no",
+        status: "active",
+      });
+    }
+  };
+
   return (
     <>
       <div id="wallpaper" style={{ overflowY: "scroll" }}>
@@ -91,7 +184,7 @@ const AdminPage = (props) => {
           </div>
         </div>
 
-        <div style = {{marginTop: '4rem'}}>
+        <div style={{ marginTop: "4rem" }}>
           <h4 id="statsText" className="frosted">
             Doctors
           </h4>
@@ -105,31 +198,45 @@ const AdminPage = (props) => {
                   <th>Gender</th>
                   <th>Specialization</th>
                   <th>Years Of Experience</th>
+                  <th>Requested</th>
                   <th>Status</th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td>
-                    <button className="btn btn-danger">Delete</button>
-                  </td>
-                  <td>
-                    <button className="btn btn-warning">Toggle Status</button>
-                  </td>
-                </tr>
+                {sharedData.doctors.map((data) => (
+                  <tr>
+                    <td>{data.id}</td>
+                    <td>{data.name}</td>
+                    <td>{data.age}</td>
+                    <td>{data.gender}</td>
+                    <td>{data.specialization}</td>
+                    <td>{data.experience}</td>
+                    <td>{data.request}</td>
+                    <td>{data.status}</td>
+                    <td>
+                      <button
+                        className="btn btn-danger"
+                        onClick={() => deleteDoctor(data.id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        className="btn btn-warning"
+                        onClick={() => toggleDoctor(data)}
+                      >
+                        Toggle Status
+                      </button>
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
         </div>
 
-        <div style = {{marginTop: '2rem'}}>
+        <div style={{ marginTop: "2rem" }}>
           <h4 id="statsText" className="frosted">
             Patients
           </h4>
@@ -145,16 +252,23 @@ const AdminPage = (props) => {
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td>
-                    <button className="btn btn-danger">Delete</button>
-                  </td>
-                </tr>
+                {sharedData.patients.map((data) => (
+                  <tr>
+                    <td>{data.id}</td>
+                    <td>{data.name}</td>
+                    <td>{data.age}</td>
+                    <td>{data.gender}</td>
+                    <td>{data.ailment}</td>
+                    <td>
+                      <button
+                        className="btn btn-danger"
+                        onClick={() => deletePatient(data.id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
@@ -170,7 +284,9 @@ const AdminPage = (props) => {
                 type="text"
                 className="form-control"
                 id="doctorID"
+                name="doctorID"
                 onChange={handleInputChange}
+                value = {doctorId}
               />
             </div>
           </form>
@@ -183,17 +299,23 @@ const AdminPage = (props) => {
                 <th>Gender</th>
                 <th>Specialization</th>
                 <th>Years Of Experience</th>
+                <th>Requested</th>
                 <th>Status</th>
               </tr>
             </thead>
             <tbody>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
+              {doctorIdData.map((data) => (
+                <tr>
+                  <td>{data.id}</td>
+                  <td>{data.name}</td>
+                  <td>{data.age}</td>
+                  <td>{data.gender}</td>
+                  <td>{data.specialization}</td>
+                  <td>{data.experience}</td>
+                  <td>{data.request}</td>
+                  <td>{data.status}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
@@ -208,7 +330,9 @@ const AdminPage = (props) => {
                 type="text"
                 className="form-control"
                 id="patientID"
+                name="patientID"
                 onChange={handleInputChange}
+                value = {patientId}
               />
             </div>
           </form>
@@ -223,22 +347,17 @@ const AdminPage = (props) => {
               </tr>
             </thead>
             <tbody>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
+            {patientIdData.map((data) => (
+                  <tr>
+                    <td>{data.id}</td>
+                    <td>{data.name}</td>
+                    <td>{data.age}</td>
+                    <td>{data.gender}</td>
+                    <td>{data.ailment}</td>
+                  </tr>
+                ))}
             </tbody>
           </table>
-        </div>
-
-        <div id="activateDoctor">
-          <h1 className="frosted" style={{ padding: "1rem" }}>
-            Doctors requesting activation.
-          </h1>
-          <div id="docCards">
-            <DoctorCardAvailable />
-          </div>
         </div>
       </div>
     </>
