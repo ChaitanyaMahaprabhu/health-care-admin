9f93c6fc77a34f55698363a504eab6f2160efe00
chaitanya-mahaprabhu-kanini <chaitanya.kanini@gmail.com>
2023-07-02 20:41:01 +0530

    Table to view and delete patient data, getting patient data by patient ID created in AdminPage.

--- src/components/AdminPage/AdminPage.jsx
+++ src/components/AdminPage/AdminPage.jsx
@@ -3,9 +3,63 @@ import { AdminNavbar } from "../Navbars/AdminNavbar";
 import { DoubleStats } from "../Statistics/DoubleStats";
 import { SingleStats } from "../Statistics/SingleStats";
 import { DoctorCardAvailable } from "../DoctorCard/DoctorCard";
+import { context } from "../../context/SharedData";
+import { useContext } from "react";
+import { useState, useEffect } from "react";
 
 const AdminPage = (props) => {
-  const arr = [1, 2, 3];
+  const con = useContext(context);
+
+  const [doctorId, setDoctorId] = useState("");
+  const [patientId, setPatientId] = useState("");
+  const [patientIdData, setPatientIdData] = useState([{}]);
+  const [doctorIdData, setDoctorIdData] = useState([{}]);
+  const [toggle, setToggle] = useState({});
+
+  useEffect(() => {
+    fetchDoctorById();
+  }, [doctorId]);
+
+  useEffect(() => {
+    fetchPatientById();
+  }, [patientId]);
+
+  const fetchDoctorById = async () => {
+    try {
+      const response = await fetch(
+        `https://localhost:7210/api/Doctors/${doctorId}`
+      );
+      if (response.ok) {
+        const data = await response.json();
+        setDoctorIdData([data]);
+      } else {
+        console.error("Error fetching doctors:", response.statusText);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const fetchPatientById = async () => {
+    try {
+      const response = await fetch(
+        `https://localhost:7210/api/Patients/${patientId}`
+      );
+      if (response.ok) {
+        const data = await response.json();
+        setPatientIdData([data]);
+      } else {
+        console.error("Error fetching patients:", response.statusText);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    if (event.target.name === "doctorID") setDoctorId(event.target.value);
+    else setPatientId(event.target.value);
+  };
 
   return (
     <>
@@ -37,37 +91,73 @@ const AdminPage = (props) => {
           </div>
         </div>
 
-        <div className="container-fluid bg-light" id="getDoctors">
-          <table className="table">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Age</th>
-                <th>Gender</th>
-                <th>Specialization</th>
-                <th>Years Of Experience</th>
-                <th>Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td>
-                  <button className="btn btn-danger">Delete</button>
-                </td>
-                <td>
-                  <button className="btn btn-warning">Toggle Status</button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+        <div style = {{marginTop: '4rem'}}>
+          <h4 id="statsText" className="frosted">
+            Doctors
+          </h4>
+          <div className="container-fluid bg-light" id="getDoctors">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Age</th>
+                  <th>Gender</th>
+                  <th>Specialization</th>
+                  <th>Years Of Experience</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td>
+                    <button className="btn btn-danger">Delete</button>
+                  </td>
+                  <td>
+                    <button className="btn btn-warning">Toggle Status</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+
+        <div style = {{marginTop: '2rem'}}>
+          <h4 id="statsText" className="frosted">
+            Patients
+          </h4>
+          <div className="container-fluid bg-light" id="getDoctors">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Age</th>
+                  <th>Gender</th>
+                  <th>Ailment</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td>
+                    <button className="btn btn-danger">Delete</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
 
         <div className="container-fluid frosted" id="getDoctorById">
@@ -76,7 +166,12 @@ const AdminPage = (props) => {
               <label htmlFor="doctorID" className="form-label">
                 Doctor ID
               </label>
-              <input type="text" className="form-control" id="doctorID" />
+              <input
+                type="text"
+                className="form-control"
+                id="doctorID"
+                onChange={handleInputChange}
+              />
             </div>
           </form>
           <table className="table mt-5">
@@ -103,12 +198,46 @@ const AdminPage = (props) => {
           </table>
         </div>
 
+        <div className="container-fluid frosted" id="getDoctorById">
+          <form>
+            <div className="mb-3">
+              <label htmlFor="patientID" className="form-label">
+                Patient ID
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="patientID"
+                onChange={handleInputChange}
+              />
+            </div>
+          </form>
+          <table className="table mt-5">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Gender</th>
+                <th>Ailment</th>
+              </tr>
+            </thead>
+            <tbody>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tbody>
+          </table>
+        </div>
+
         <div id="activateDoctor">
-          <h1 className="frosted" style = {{padding: '1rem'}}>Doctors requesting activation.</h1>
-          <div id = "docCards">
-            {arr.map((value) => (
-              <DoctorCardAvailable />
-            ))}
+          <h1 className="frosted" style={{ padding: "1rem" }}>
+            Doctors requesting activation.
+          </h1>
+          <div id="docCards">
+            <DoctorCardAvailable />
           </div>
         </div>
       </div>
--- src/context/SharedData.jsx
+++ src/context/SharedData.jsx
@@ -1,12 +1,53 @@
 import { createContext } from "react";
+import { useState, useEffect } from "react";
 
 const context = createContext();
 const SharedData = (props) => {
     const users = ["Patient", "Doctor", "Admin"];
     const genders = ["Male", "Female"];
+
+    const [doctors, setDoctors] = useState([]);
+    const [patients, setPatients] = useState([]);
+
+    useEffect(() => {
+        getDoctors();
+        getPatients();
+      }, []);
+      
+      
+      const getDoctors = async () => {
+        try {
+          const response = await fetch("https://localhost:7210/api/Doctors");
+          if (response.ok) {
+            const data = await response.json();
+            setDoctors(data);
+          } else {
+            console.error("Error fetching doctors:", response.statusText);
+          }
+        } catch (e) {
+          console.log(e);
+        }
+      };
+
+      const getPatients = async () => {
+        try {
+          const response = await fetch("https://localhost:7210/api/Patients");
+          if (response.ok) {
+            const data = await response.json();
+            setPatients(data);
+          } else {
+            console.error("Error fetching patients:", response.statusText);
+          }
+        } catch (e) {
+          console.log(e);
+        }
+      };
+      
     const data = {
         users: users,
-        genders: genders
+        genders: genders,
+        doctors: doctors,
+        patients: patients
     };
     
     return(
