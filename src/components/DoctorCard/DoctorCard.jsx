3224df78d44685f2965eaa215f777b4c0b7b1a0e
chaitanya-mahaprabhu-kanini <chaitanya.kanini@gmail.com>
2023-07-03 17:04:37 +0530

    Images are now dynamically generated.

--- src/components/DoctorCard/DoctorCard.jsx
+++ src/components/DoctorCard/DoctorCard.jsx
@@ -1,12 +1,11 @@
 import "./DoctorCard.css";
-import image1 from "../../assets/images/1.png";
 import { useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import image from "../../assets/images/1.png";
 
 const DoctorCardAvailable = (props) => {
   const path = "../../assets/images/" + props.specialization + ".jpeg";
-  const [doctorIdData, setDoctorIdData] = useState([{}]);
   const [doctor, setDoctor] = useState({
     id: props.id,
     name: `${props.name}`,
@@ -20,18 +19,21 @@ const DoctorCardAvailable = (props) => {
 
   const clickHandler = async () => {
     try {
-      const response = await fetch(`https://localhost:7261/api/Doctors/${props.id}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(doctor),
-      });
+      const response = await fetch(
+        `https://localhost:7261/api/Doctors/${props.id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(doctor),
+        }
+      );
       if (response.ok) {
         console.log("Doctor has been assigned!");
         alert("Doctor has been assigned!");
         console.log(doctor);
-        window.location = '/';
+        window.location = "/";
       } else {
         console.error("Error assigning doctor:", response.statusText);
         console.log(doctor);
@@ -41,10 +43,12 @@ const DoctorCardAvailable = (props) => {
     }
   };
 
-
   return (
     <div id="doctorCard" className="frosted">
-      <img id="doctorImage" src={path}></img>
+      <img
+        id="doctorImage"
+        src={require(`../../assets/images/${props.specialization}.jpeg`)}
+      ></img>
       <div id="doctorInfo">
         <div>
           <h1>Dr. {props.name}</h1>
@@ -58,7 +62,9 @@ const DoctorCardAvailable = (props) => {
           <h4>Age : 22</h4>
         </div>
 
-        <button className="btn btn-primary btn-lg mt-5" onClick = {clickHandler}>Choose</button>
+        <button className="btn btn-primary btn-lg mt-5" onClick={clickHandler}>
+          Choose
+        </button>
       </div>
     </div>
   );
@@ -67,7 +73,11 @@ const DoctorCardAvailable = (props) => {
 const DoctorCardUnavailable = (props) => {
   return (
     <div id="doctorCard" className="frosted">
-      <div id="doctorImage" style={{ background: `url(${image1})` }}></div>
+      <img
+        id="doctorImage"
+        src={require(`../../assets/images/${props.specialization}.jpeg`)}
+      ></img>
+
       <div id="doctorInfo">
         <div>
           <h1>Dr. {props.name}</h1>
